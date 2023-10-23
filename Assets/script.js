aff101e15952d6e1970184c2dcff4ac1a31ad786
Lady Godiva <watson.amber.denise@gmail.com>
2023-10-23 14:10:47 -0700

    updated html and js code to include additional time blocks and effective cod to assign style class conditionally and add text to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,100 +1,54 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var saveBtn = document.querySelectorAll('btn saveBtn col-2 col-md-1');
+
 var date = dayjs('#currentDay');
 var reformat = dayjs().format('dddd, MMMM D YYYY');
 var time = dayjs().format('h');
-console.log(time);
-var calendarTime = $(["9", "10", "11", "12", "1", "2", "3", "4", "5"]);
-var textArea = $("#textArea");
-var color = $('.row time-block');
-// var hidden = document.querySelectorAll('fas fa-save');
+var calendarTime = $(".time-block");
+var saveBtn = $(".saveBtn")
+
 
 $(document).ready(function () {
-    // $(function (events) {
 
+    //function to display current day in preferred format on the area of the page designated, #currentDay.
     date = setInterval(function () {
         $('#currentDay').text(reformat);
     }, 1000);
 
+    //function to iterate over every row time-block and associated id to determine which style class displays based on conditional statement below.
     currentTime = function () {
         $('.row time-block').each(function () {
 
         }
         )
     }
-    console.log('calendarTime:' + JSON.stringify(calendarTime));
-    console.log('time:' + time)
-    for (var i = 0; i < JSON.stringify(calendarTime); i++) {
-        var id = $(this).attr("#id");
-        var splitId = id.split('-');
-        // textArea.push({ userInput: textArea.value })
-        if (splitId == calendarTime) {
-            color.addClass('.present');
-        } else if (splitId > calendarTime) {
-            color.addClass('.future');
+    //function to iterate over each id attribute 
+    calendarTime.each(function () {
+        var id = $(this).attr("id");
+        //variables to choose, split, and display the second value of the id were created so that it could be compared to current time. Because the split id was not a number, I had to use parseInt to return that type of value.
+        var timeBlockHour = parseInt(id.split('-')[1]);
+        var currentHour = dayjs().hour();
+        //created conditional statement that compared currentHour to the timeBlockHour we created above to display classes accordingly.
+        if (timeBlockHour === currentHour) {
+            $(this).addClass("present");
+        } else if (timeBlockHour < currentHour) {
+            $(this).addClass("past");
         } else {
-            // textArea.css('background-color', 'd3d3d3');
-            color.addClass('.past');
-            document();
+            $(this).addClass("future");
         }
-
-    }
-
+        var savedNote = localStorage.getItem(id) || '';
+        $(this).children(".description").val(savedNote);
+    })
     currentTime();
+
+    //function to save contents of the textbox area using the saveBtn to traverse the DOM and identify attribues/values.
+    var printEvent = function (event) {
+        event.preventDefault();
+        var saveBtn = $(event.target);
+        var noteKey = saveBtn.parent().attr("id");
+        var noteText = saveBtn.siblings(".description").val();
+        //set those values to local storage for continued access.
+        localStorage.setItem(noteKey, noteText);
+    }
+    //created a click event to call the printEvent function so that text entered by the user would be saved to local storage, even after a refresh.
+    saveBtn.on('click', printEvent)
+
 });
-
-
-function enterEvents(event) {
-    var saveBtn = $(event.target);
-    var storage = $('.storage');
-    var eventList = $('#textArea').val();
-    var eventListItemEl = $('col-8 col-md-10 description');
-    saveBtn.getItem(eventList).append(storage);
-    eventListItemEl.text(eventList);
-    // eventList.append(storage);
-    $('#textArea').val('');
-
-    if (event.target.saveBtn === 'click') {
-        textArea = JSON.parse(localStorage.getItem("#textArea")) || []
-
-        userInput.textContent = textArea.value;
-
-        localStorage.setItem("#textArea", JSON.stringify(textArea));
-
-    }
-
-    for (var i = 0; i < textArea.length; i++) {
-        textArea.push({ userInput: textArea.value })
-
-    }
-    eventList.on('click', '.btn saveBtn col-2 col-md-1', enterEvents)
-}
-
-
-
-
-
-
-
-//set each button to have a data attribute.
-//on a click it deletes the item in the list.
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page
--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # Event-Planner
+## Description
+
+The purpose of this challenge was to deploy a web application and event plannr with interactive components. The project demonstrated dynamic HTML and CSS code, using JQuery. 
+
+Users should see the current date at the top of the planner and depending on the time of day, see areas designated (by color) for the present, past, and future time. Each text area can be used to enter an event and then the entry will be saved to local storage for future access. 
+
+The acceptance criteria were as follows: 
+GIVEN I am taking a code quiz
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+All of the acceptance criteria were met and the site functions as expected.
+
+I learned how to navigate the DOM using jquery to create dynamic pages. [Web APIs present a unique challenge wherein aspects of the HTML can be called on to perform functions based on needs. Furthermore, I realized (after the fact) how important naming conventions can be. In many cases the variables declared were quite similar, so much as that they were hard to distinguish at times. Specificity is critical when utilizing the DOM structure. Getting the arrays to work in the way I needed them too posed a unique challenge. -UPDATE] I found that reformatting made for easier reference later on - I will be continue to follow that model in the future. Finally, I realize how vital notation and formatting code are to developing applications. I am still working to make this a natural implementation piece during the build process. 
+## Usage
+
+The html, css, and js files can be used to understand the dynamic code that supports the application. A link to the mockup is listed below, for your convenience. 
+![Event Planner Mockup](./assets/images/Quiz%20Mockup.png)
+
+
+## Credits
+
+The project was completed with help from the course materials and assistance from Bootcamp tutor, Erik Hirsch and AskBSC support - Zack.
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [Amber Watson]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
